6031ffa853fe8236773473ed371e5fed0ccd6d03
sahrialardians <sahrialardians@gmail.com>
2020-03-17 21:45:36 +0700

    create data json and passing data

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="app">
-    
+    <h2>What I do Today?</h2>
+    <div class="home-work">
+
+      <div v-for="taskMe in tasks" v-bind:key="taskMe.id" class="tasks">
+        <p>{{ taskMe.nameTask }}</p>
+      </div>
+
+    </div>
   </div>
 </template>
 
@@ -8,6 +15,28 @@
 
 export default {
   name: 'App',
+  data: function(){
+    return{
+      tasks: [
+        {
+          id: 1,
+          nameTask: 'Washing clothes'
+        },
+        {
+          id: 2,
+          nameTask: 'Learning online course'
+        },
+        {
+          id: 3,
+          nameTask: 'Repairing motorcycle'
+        },
+        {
+          id: 4,
+          nameTask: 'Creating study case'
+        },
+      ]
+    };
+  },
   components: {
     
   }
